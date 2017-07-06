0a3fc765af75f82307497ed1eb63250a6d26c2a4
Васильев Александр <leonbkone@yandex.ru>
2017-07-06 22:32:16 +0500

    Add files via upload

---
+++ GetPhotosFromSN.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Square.Picasso;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+namespace XamarinGoogleMapDemo
+{
+    [Activity(Label = "GetPhotosFromSN")]
+    public class GetPhotosFromSN : Activity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.GetPhotosFromSN);
+
+            double lat = 46.7404283551898, lng = 28.8811472434254;
+            string request = "https://api.vk.com/method/photos.search?lat=";
+            string url = request +
+                Convert.ToString(lat) +
+                "&long=" +
+                Convert.ToString(lng) +
+                "&v=5.65";
+
+            //Serialize
+            var photo = await GetPhotoList(url);
+            //string asdas = "23423";
+
+
+            //Add photos in layout
+            Picasso.With(this)
+                .Load(photo.photo_130.ToString())
+                .Into(FindViewById<ImageView>(Resource.Id.imageView1));
+        }
+
+        private async Task<Photo> GetPhotoList(string url)
+        {
+            var result = new List<Photo>();
+            var httpClient = new HttpClient(new HttpClientHandler())
+            {
+                BaseAddress = new Uri(url)
+            };
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var resp = await httpClient.GetAsync(url).ConfigureAwait(false);
+
+            if (resp.IsSuccessStatusCode)
+            {
+                var content = resp.Content;
+                string task = await content.ReadAsStringAsync().ConfigureAwait(false);
+
+                return JsonConvert.DeserializeObject<Photo>(task);
+            }
+            return new Photo();
+        }
+
+        public class Photo
+        {
+            public string photo_130 { get; set; }
+
+            public List<Photo> photo { get; set; }
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+using System;
+using Android.Gms.Maps.Model;
+using Android.Locations;
+using Android.Runtime;
+using Android.Content;
+using Android.Graphics;
+
+namespace XamarinGoogleMapDemo
+{
+    [Activity(Label = "XamarinGoogleMapDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, IOnMapReadyCallback, GoogleMap.IOnMapLongClickListener
+    {
+        GoogleMap map;
+
+        public void OnMapLongClick(LatLng point)
+        {
+            CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(point, 15);
+            map.MoveCamera(camera);
+            string LatLong = "Latitude: ";
+            LatLong += Convert.ToString(point.Latitude);
+            LatLong += "\nLongitude: ";
+            LatLong += Convert.ToString(point.Longitude);
+            MarkerOptions options = new MarkerOptions()
+                .SetPosition(point)
+                .SetTitle(LatLong);
+
+            map.AddMarker(options);
+
+            StartActivity(typeof(GetPhotosFromSN)); 
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            map = googleMap;
+
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.SetPosition(new LatLng(16.03, 108));
+            markerOptions.SetTitle("My position");
+            googleMap.AddMarker(markerOptions);
+
+            //Optional
+            googleMap.UiSettings.ZoomControlsEnabled = true;
+            googleMap.UiSettings.CompassEnabled = true;
+            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
+
+            //LongClick on Map
+            map.SetOnMapLongClickListener(this);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+        }
+    }
+}
+
